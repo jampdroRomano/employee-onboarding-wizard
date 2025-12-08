6e84553f154aa2ca3dfaa36baad922a382a9dca0
João Pedro Vieira dos Santos Romano <jpvs06x0805@gmail.com>
2025-12-08 17:50:23 -0300

    Initial commit

---
+++ src/config/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbX46Lt_ExVi534l98WaoU5EeKYpZZhC4",
+  authDomain: "employeeregistration-7aec5.firebaseapp.com",
+  projectId: "employeeregistration-7aec5",
+  storageBucket: "employeeregistration-7aec5.firebasestorage.app",
+  messagingSenderId: "561685912204",
+  appId: "1:561685912204:web:1e988b8fca9b0cbaedb9a9"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
